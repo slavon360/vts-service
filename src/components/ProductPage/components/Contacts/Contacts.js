ae7d08f150759c3641f612ee93c49fd6892aed08
viacheslav liakh <slavon360@gmail.com>
2019-08-01 00:29:14 +0300

    menu mobile minor fixes

--- src/components/ProductPage/components/Contacts/Contacts.js
+++ src/components/ProductPage/components/Contacts/Contacts.js
@@ -6,16 +6,18 @@ const Contacts = ({ contacts }) => (
         <div className={styles.ContactsWrp}>
             <h3>Наши контакты:</h3>
             <div className={styles.ContactsArea}>
-                {contacts.map(({ email, телефон, image: { secure_url } = {}, _id }) => (
-                    <div
-                        key={_id}
-                        className={styles.ContactItem}
-                    >
-                        {телефон &&
-                            <ContactPhone телефон={телефон} secure_url={secure_url} />
+                {contacts.map(({ email, телефон, image: { secure_url } = {}, _id }) => {
+                        if (телефон) {
+                            return (<div
+                                        key={_id}
+                                        className={styles.ContactItem}
+                                    >
+                                    <ContactPhone телефон={телефон} secure_url={secure_url} />
+
+                                    </div>)
                         }
-                    </div>
-                ))}
+                        return null;
+                    })}
             </div>
         </div>
     );
