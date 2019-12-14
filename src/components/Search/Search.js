68cde9e7a2cb7f59ca65b9f829f2b9870a4188d4
viacheslav liakh <slavon360@gmail.com>
2019-12-15 00:38:28 +0200

    search field updating (make active after toggle)

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import cx from 'classnames';
 import SearchIcon from '../Icons/SearchIcon';
 import { SearchResults, NotFound } from './components';
@@ -7,50 +7,68 @@ import PreloaderBar from '../PreloaderBar';
 import styles from './Search.module.scss';
 import '../../App.css'
 
-const Search = ({
-    contacts,
-    toggleSearch,
-    searchActive,
-    searchProducts,
-    searchedText,
-    searchedProducts,
-    searchedProductsLoading,
-    showSearchedProducts
-}) => (
-        <div className={cx(
-            styles.Search,
-            {
-                ['SearchActive']: searchActive,
-                [styles.Inactive]: !searchActive,
-                ['SearchInactive']: !searchActive
-            })}>
-            <button
-                onClick={toggleSearch}
-                className={styles.SearchIcon}
-            >
-                <SearchIcon style={{ fontSize: '20px' }}/>
-            </button>
+class Search extends Component {
+    onToggleSearch = () => {
+        console.log(this.refs.SearchField.focus());
+        this.props.toggleSearch();
+    }
+    keyPressHandling = (event) => {
+        const { searchProducts, searchedText } = this.props;
+        const simmulateEvtTarget = { target: { value: searchedText } };
+        if (event.key === 'Enter'){
+            searchProducts(simmulateEvtTarget);
+        }
+    }
+    render () {
+        const {
+            contacts,
+            searchActive,
+            searchProducts,
+            searchedText,
+            searchedProducts,
+            searchedProductsLoading,
+            showSearchedProducts
+        } = this.props;
+
+        return (
             <div className={cx(
-            styles.SearchInputContainer,
-            { [styles.Active]: searchActive, [styles.Inactive]: !searchActive })}>
-                <input
-                    className={styles.SearchInput}
-                    value={searchedText}
-                    type="text"
-                    placeholder="Поиск..."
-                    autoComplete="off"
-                    onChange={searchProducts}
-                />
+                styles.Search,
+                {
+                    ['SearchActive']: searchActive,
+                    [styles.Inactive]: !searchActive,
+                    ['SearchInactive']: !searchActive
+                })}>
+                <button
+                    onClick={this.onToggleSearch}
+                    className={styles.SearchIcon}
+                >
+                    <SearchIcon style={{ fontSize: '20px' }}/>
+                </button>
+                <div className={cx(
+                styles.SearchInputContainer,
+                { [styles.Active]: searchActive, [styles.Inactive]: !searchActive })}>
+                    <input
+                        ref="SearchField"
+                        className={styles.SearchInput}
+                        value={searchedText}
+                        type="text"
+                        placeholder="Поиск..."
+                        autoComplete="off"
+                        onChange={searchProducts}
+                        onKeyPress={this.keyPressHandling}
+                    />
+                </div>
+                {searchedProducts && searchedProducts.length ?
+                    <SearchResults
+                        results={searchedProducts}
+                        showSearchedProducts={showSearchedProducts}
+                    />
+                    : <NotFound searchedText={searchedText} searchedProducts={searchedProducts} contacts={contacts} />
+                }
+                {searchedProductsLoading ? <PreloaderBar /> : <div />}
             </div>
-            {searchedProducts && searchedProducts.length ?
-                <SearchResults
-                    results={searchedProducts}
-                    showSearchedProducts={showSearchedProducts}
-                />
-                : <NotFound searchedText={searchedText} searchedProducts={searchedProducts} contacts={contacts} />
-            }
-            {searchedProductsLoading ? <PreloaderBar /> : <div />}
-        </div>
-    );
+        )
+    }
+};
 
 export default Search;
