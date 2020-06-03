072b0bb00f355f8df2ef0b3f7093a740dd4a8cb4
slavon360 <slavon360@github.com>
2020-06-03 23:45:35 +0300

    move description to end

--- src/components/ProductPage/components/Details/components/Properties/Properties.js
+++ src/components/ProductPage/components/Details/components/Properties/Properties.js
@@ -5,36 +5,57 @@ import CheckIcon from '../../../../../Icons/CheckIcon';
 
 import styles from './Properties.module.scss';
 
-const Properties = ({ properties, sizes }) => (
-        <div className={styles.Properties}>
-            <div className={styles.Head}>Характеристики</div>
-            <div className={styles.PropertiesContent}>
-                {properties.map(item => {
-                    if (sizes[item[0]] === item[1]) return null;
-                    return item[1] ? (
-                        <div
-                            key={item[0]}
-                            className={cx(styles.KeyValueWrp, { [styles.Description]: item[0] === 'Описание' })}
-                        >
-                            <div className={styles.Key}>{item[0]}</div>
-                            {item[0] === 'Описание' ?
-                                <div className={styles.Value} dangerouslySetInnerHTML={{ __html: item[1] }}></div> :
-                                <div className={styles.Value}>{ item[1] === true ? <CheckIcon /> : item[1]}</div>
-                            }
-                        </div>
-                        ) : null
-                    }
-                )}
-            </div>
-        </div>
-    );
+const moveDescriptionToEnd = properties => {
+	let descr_item;
+	
+	const upd_properties = properties.reduce((result, current, index) => {
+		if (current[0] !== 'Описание') {
+			result.push(current);
+		} else {
+			descr_item = properties.splice(index, 1);
+		}
+		return result;
+	}, []);
+
+	upd_properties.push(...descr_item);
+
+	return upd_properties;
+}
+
+const Properties = ({ properties, sizes }) => {
+	const upd_properties = moveDescriptionToEnd(properties);
+
+	return (
+		<div className={styles.Properties}>
+			<div className={styles.Head}>Характеристики</div>
+			<div className={styles.PropertiesContent}>
+				{upd_properties.map(item => {
+					if (sizes[item[0]] === item[1]) return null;
+					return item[1] ? (
+						<div
+							key={item[0]}
+							className={cx(styles.KeyValueWrp, { [styles.Description]: item[0] === 'Описание' })}
+						>
+							<div className={styles.Key}>{item[0]}</div>
+							{item[0] === 'Описание' ?
+								<div className={styles.Value} dangerouslySetInnerHTML={{ __html: item[1] }}></div> :
+								<div className={styles.Value}>{ item[1] === true ? <CheckIcon /> : item[1]}</div>
+							}
+						</div>
+						) : null
+					}
+				)}
+			</div>
+		</div>
+	);
+}
 
 export default Properties;
 
 Properties.propTypes = {
-    properties: PropTypes.array
+	properties: PropTypes.array
 }
 
 Properties.defaultProps = {
-    properties: []
+	properties: []
 }
